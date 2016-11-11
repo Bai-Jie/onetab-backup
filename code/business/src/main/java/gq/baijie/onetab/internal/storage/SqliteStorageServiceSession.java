607c6ce6ecab941918b4b682386f61dbd5dcb7af
Bai Jie <baijie1991@gmail.com>
2016-11-11 09:38:25 +0800

    implement SqliteStorageServiceSession.retrieve()

--- code/business/src/main/java/gq/baijie/onetab/internal/storage/SqliteStorageServiceSession.java
+++ code/business/src/main/java/gq/baijie/onetab/internal/storage/SqliteStorageServiceSession.java
@@ -8,9 +8,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Types;
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import gq.baijie.onetab.ProgressOrResult;
 import gq.baijie.onetab.Results;
@@ -53,13 +55,26 @@ public class SqliteStorageServiceSession implements StorageServiceSession {
         return;
       }
       subscriber.onNext(ProgressOrResult.result(Results.<Void, Throwable>succeed(null)));
+      subscriber.onCompleted();
     });
   }
 
   @Nonnull
   @Override
   public Observable<ProgressOrResult<WebArchive, Throwable>> retrieve() {
-    return null;
+    return Observable.create(subscriber -> {
+      if (subscriber.isUnsubscribed()) {
+        return;
+      }
+      final WebArchive result = doRetrieve();
+      if (subscriber.isUnsubscribed()) {
+        return;
+      }
+      if (result != null) {
+        subscriber.onNext(ProgressOrResult.result(Results.<WebArchive, Throwable>succeed(result)));
+      }
+      subscriber.onCompleted();
+    });
   }
 
   private void doSave(@Nonnull WebArchive webArchive) {
@@ -70,7 +85,7 @@ public class SqliteStorageServiceSession implements StorageServiceSession {
       statement.executeUpdate("drop table if exists section");
       statement.executeUpdate("create table section (storage_id integer PRIMARY KEY, id string, create_date integer DEFAULT NULL)");
       statement.executeUpdate("drop table if exists item");
-      statement.executeUpdate("create table item (storage_id integer PRIMARY KEY, section_storage_id integer, id string, link string, title string)");;
+      statement.executeUpdate("create table item (storage_id integer PRIMARY KEY, section_storage_id integer, id string, link string, title string)");
       for(WebArchive.Section section:webArchive.getSections()) {
         saveSection(section, connection);
       }
@@ -114,6 +129,56 @@ public class SqliteStorageServiceSession implements StorageServiceSession {
         statement.setString(4, item.getTitle());
         statement.executeUpdate();
       }
+    }
+  }
+
+  @Nullable
+  private WebArchive doRetrieve() {
+    try (final Connection connection = DriverManager.getConnection(databaseUrl);
+         final Statement statement = connection.createStatement()) {
+      connection.setAutoCommit(false);
+
+      final WebArchive.WebArchiveBuilder builder = WebArchive.builder();
+      // retrieve sections
+      ResultSet rs = statement.executeQuery("select storage_id, id, create_date from section");
+      while (rs.next()) {
+        final WebArchive.SectionBuilder section = builder.section();
+        section.setId(rs.getString(2));
+        final long time = rs.getLong(3);
+        if (rs.wasNull()) {
+          section.setCreateDate(null);
+        } else {
+          section.setCreateDate(new Date(time));
+        }
+        // retrieve the current section's items
+        long sectionStorageId = rs.getLong(1);
+        retrieveItems(sectionStorageId, section, connection);
+      }
+      rs.close();
+
+      connection.commit();
+
+      return builder.build();
+    } catch (SQLException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  private void retrieveItems(
+      long sectionStorageId, WebArchive.SectionBuilder section, @Nonnull Connection connection)
+      throws SQLException {
+    try (final PreparedStatement statement = connection.prepareStatement(
+        "select id, link, title from item where section_storage_id is ?")) {
+      statement.setLong(1, sectionStorageId);
+      final ResultSet resultSet = statement.executeQuery();
+      while (resultSet.next()) {
+        final WebArchive.ItemBuilder item = section.item();
+        item.setId(resultSet.getString(1));
+        item.setLink(resultSet.getString(2));
+        item.setTitle(resultSet.getString(3));
+      }
+      resultSet.close();
     }
   }
 
